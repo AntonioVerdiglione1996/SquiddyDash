fd816bf9c153d49e05b6834d99e58f107560f511
Matteo Lorenzo Nasci <Matteo.Lorenzo.Nasci@hotmail.com>
2018-12-01 19:12:57 +0100

    Moved some stuff, modified InGameCurrency system (now gamecurrency and premiumcurrency)

--- Assets/ScriptableObject/Scripts/Global/InGameCurrency.cs
+++ Assets/ScriptableObject/Scripts/Global/InGameCurrency.cs
@@ -6,11 +6,34 @@ using System;
 [CreateAssetMenu(menuName = "InGameCurrency")]
 public class InGameCurrency : ScriptableObject
 {
-    public int Currency;
-
-    public void IncreaseAmount(int amount)
+    public long GameCurrency { get { return gameCurrency; } }
+    [SerializeField]
+    private long gameCurrency;
+    public bool ModifyGameCurrencyAmount(long toSum)
     {
-        Currency += amount;
+        long result = gameCurrency + toSum;
+        if(result < 0)
+        {
+            return false;
+        }
+
+        gameCurrency = result;
         SerializerHandler.SaveJsonFromInstance(SerializerHandler.PersistentDataDirectoryPath, "Currency.json", this, true);
+        return true;
+    }
+    public long FetchUpdatedPremiumCurrencyFromServer()
+    {
+        //TODO: gestire premiumC lato server forse? (per evitare manomissioni lato client).Problema di questo approccio è che una persona offline non possa usare i suoi PremiumCurrency. Dovrebbe essere gestito come operazione asyncrona
+        return 0;
+    }
+    public bool UsePremiumCurrency(long amountUsed)
+    {
+        if(amountUsed <= 0)
+        {
+            return false;
+        }
+
+        //TODO: inform server of this operation. Dovrebbe essere gestito come operazione asyncrona
+        return false;
     }
 }
