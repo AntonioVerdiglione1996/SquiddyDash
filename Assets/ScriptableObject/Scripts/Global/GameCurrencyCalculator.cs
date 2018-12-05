4082ae86877c68d5f7c8fd0bd6fa2f6866610f7c
Matteo Lorenzo Nasci <Matteo.Lorenzo.Nasci@hotmail.com>
2018-12-05 20:58:12 +0100

    Added gamecurrency earning logic when game finishes

---
+++ Assets/ScriptableObject/Scripts/Global/GameCurrencyCalculator.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+[CreateAssetMenu]
+public class GameCurrencyCalculator : ScriptableObject
+{
+    [SerializeField]
+    protected long BaseAmount = 15;
+    [SerializeField]
+    protected double ScoreMultiplicator = 0.5f;
+    public virtual long CalculateCurrencyIncreaseAmount(ScoreSystem ScoreSystem)
+    {
+        return BaseAmount + (long)(ScoreSystem.Score * ScoreMultiplicator);
+    }
+}
