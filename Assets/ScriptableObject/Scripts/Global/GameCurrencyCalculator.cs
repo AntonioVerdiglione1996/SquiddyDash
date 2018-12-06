4ad9b50649f962ce2e8943e5a625dd0e6a7f1007
Matteo Lorenzo Nasci <Matteo.Lorenzo.Nasci@hotmail.com>
2018-12-06 12:39:29 +0100

    minot changes

--- Assets/ScriptableObject/Scripts/Global/GameCurrencyCalculator.cs
+++ Assets/ScriptableObject/Scripts/Global/GameCurrencyCalculator.cs
@@ -5,7 +5,7 @@ public class GameCurrencyCalculator : ScriptableObject
     [SerializeField]
     protected long BaseAmount = 15;
     [SerializeField]
-    protected double ScoreMultiplicator = 0.5f;
+    protected double ScoreMultiplicator = 0.5d;
     public virtual long CalculateCurrencyIncreaseAmount(ScoreSystem ScoreSystem)
     {
         return BaseAmount + (long)(ScoreSystem.Score * ScoreMultiplicator);
