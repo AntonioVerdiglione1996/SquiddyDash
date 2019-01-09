643e23474e803c61eeca0f9aedf01ffb635bcbd9
Matteo Lorenzo Nasci <Matteo.Lorenzo.Nasci@hotmail.com>
2019-01-09 11:31:46 +0100

    Added custom editor for scoresystem

---
+++ Assets/ScriptableObject/Scripts/EventDrivenPattern/Editor/ScoreSystemEditor.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(ScoreSystem), true)]
+public class ScoreSystemEditor : Editor
+{
+    private ScoreSystem obj;
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        if (GUILayout.Button("Raise"))
+        {
+            obj.Raise();
+        }
+        if (GUILayout.Button("Increase Score"))
+        {
+            obj.UpdateScore(1);
+        }
+        if (GUILayout.Button(obj.LocalDebugActive ? "Local debug active" : "Local debug not active"))
+        {
+            obj.LocalDebugActive = !obj.LocalDebugActive;
+        }
+        if (GUILayout.Button(GameEvent.AllDebugActive ? "Debug active" : "Debug not active"))
+        {
+            GameEvent.AllDebugActive = !GameEvent.AllDebugActive;
+        }
+    }
+    private void OnEnable()
+    {
+        obj = target as ScoreSystem;
+    }
+}
