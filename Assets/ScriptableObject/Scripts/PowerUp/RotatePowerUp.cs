c2fbf67f23c07447e77a718372098dcd8a7a346a
unknown <anto.verdi27@gmail.com>
2019-01-09 12:18:44 +0100

    power ups addes

---
+++ Assets/ScriptableObject/Scripts/PowerUp/RotatePowerUp.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatePowerUp : MonoBehaviour
+{
+
+    public float X;
+    public float Y;
+    public float Z;
+
+    public float RotationSpeed;
+
+    private void Update()
+    {
+        transform.Rotate(new Vector3(X, Y, Z) * RotationSpeed * Time.deltaTime);
+    }
+}
