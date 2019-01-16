fc09de6bd83af508f1e0b3b6710f249682830a7f
unknown <anto.verdi27@gmail.com>
2019-01-16 12:35:45 +0100

    fixed bug for build

--- Assets/Scripts/CharacterSelection/CharacterSpawner.cs
+++ Assets/Scripts/CharacterSelection/CharacterSpawner.cs
@@ -6,15 +6,9 @@ using UnityEngine.UI;
 public class CharacterSpawner : MonoBehaviour
 {
     public StoringCurrentModelToSpawn scm;
-    public Text text;
     public void Start()
     {
         GameObject go = Instantiate(scm.DownloadCurrentCharacter(), transform);
-        if (!go)
-        {
-            Application.Quit();
-        }
-        text.text = go.name;
         Destroy(this);
     }
 }
