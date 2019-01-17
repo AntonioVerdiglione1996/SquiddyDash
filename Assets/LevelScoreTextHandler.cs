949b99817ce388f3f2247118c32d2d5b772e61e0
Matteo Lorenzo Nasci <Matteo.Lorenzo.Nasci@hotmail.com>
2019-01-17 20:24:30 +0100

    Added currency ui to levelselection and characterselection

--- Assets/LevelScoreTextHandler.cs
+++ Assets/LevelScoreTextHandler.cs
@@ -17,6 +17,7 @@ public class LevelScoreTextHandler : MonoBehaviour {
         {
             Text = GetComponent<TextMeshProUGUI>();
         }
+        Update();
     }
     public void Update()
     {
--- Assets/Scripts/UI_Modifiers/CurrencyTextHandler.cs
+++ Assets/Scripts/UI_Modifiers/CurrencyTextHandler.cs
@@ -17,6 +17,7 @@ public class CurrencyTextHandler : MonoBehaviour
         {
             Text = GetComponent<TextMeshProUGUI>();
         }
+        Update();
     }
     public void Update()
     {
