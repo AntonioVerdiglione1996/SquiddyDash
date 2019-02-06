b0423421c545058408cf43bbcd2a06d5ad01dc90
unknown <anto.verdi27@gmail.com>
2019-02-06 13:14:37 +0100

    added parallax

---
+++ Assets/Scripts/Parallax/Parallax.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+
+    public float speed = 1f;
+    Vector3 newPos;
+    void Update()
+    {
+        newPos = Vector3.up * speed * Time.deltaTime;
+        if (transform.position.y > 30f)
+        {
+            transform.position = new Vector3(transform.position.x,0, transform.position.z);
+        }
+        transform.position += newPos;
+    }
+}
