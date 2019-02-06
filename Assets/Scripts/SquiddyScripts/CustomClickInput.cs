6be1da5c10ad4861f79e39b2bfefa9790a238b4e
Matteo Lorenzo Nasci <Matteo.Lorenzo.Nasci@hotmail.com>
2019-02-06 09:59:35 +0100

    Added script for custom click management

---
+++ Assets/Scripts/SquiddyScripts/CustomClickInput.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomClickInput : MonoBehaviour {
+    public BasicEvent OnClickDown;
+    public BasicEvent OnClickUp;
+    public BasicEvent OnClickPressed;
+    public List<RectTransform> Ui;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
