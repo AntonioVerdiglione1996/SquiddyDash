65a59220bf9c2804ae0a35b99faf6b9d825ed041
Matteo Lorenzo Nasci <Matteo.Lorenzo.Nasci@hotmail.com>
2019-02-17 16:29:03 +0100

    minor fix to remove accessory button, now it should remove accessory from the displaying model too

--- Assets/Scripts/UI_Modifiers/RemoveCurrentAccessory.cs
+++ Assets/Scripts/UI_Modifiers/RemoveCurrentAccessory.cs
@@ -7,8 +7,28 @@ public class RemoveCurrentAccessory : MonoBehaviour
 {
     public StoringCurrentModelToSpawn Scm;
     public AccessoryUiSpawner Spawner;
+    public Character_Manager Manager;
+    private void OnValidate()
+    {
+        Awake();
+    }
+    private void Awake()
+    {
+        if (!Spawner)
+        {
+            Spawner = FindObjectOfType<AccessoryUiSpawner>();
+        }
+        if (!Manager)
+        {
+            Manager = FindObjectOfType<Character_Manager>();
+        }
+    }
     public void RemoveAccessory()
     {
         Scm.RemoveAccessory(Spawner.CurrentType);
+        if (Manager.GetCurrentModel)
+        {
+            Manager.GetCurrentModel.CollectAndSpawnSkills(Scm.Accessories, Scm.GetAccessoriesIndices(), false);
+        }
     }
 }
