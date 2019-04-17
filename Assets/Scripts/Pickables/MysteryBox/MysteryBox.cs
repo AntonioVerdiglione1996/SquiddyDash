6b27074f6198b1c788cd12b07ad8a9ae9496df7b
unknown <anto.verdi27@gmail.com>
2019-04-17 12:40:07 +0200

    added cool stuffs on scenes, skill improver, coin.mystery box auras

--- Assets/Scripts/Pickables/MysteryBox/MysteryBox.cs
+++ Assets/Scripts/Pickables/MysteryBox/MysteryBox.cs
@@ -1,28 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System;
 public class MysteryBox : Pickable
 {
     public MysteryBoxType Type;
     public bool GenerateRandomTypeOnEnable = true;
     public MysteryBoxChances Chances;
     public GlobalEvents GlobalEvents;
+    public GameObject[] Auras;
     private void OnEnable()
     {
+        for (int i = 0; i < Auras.Length; i++)
+        {
+            GameObject itemaura = Auras[i];
+            if (itemaura)
+            {
+                itemaura.SetActive(false);
+            }
+        }
         if (GenerateRandomTypeOnEnable && Chances)
         {
+            MysteryBoxType ToSet = Chances.DefaultType;
             for (int i = 0; i < Chances.Data.Count; i++)
             {
                 MysteryBoxChancesData Data = Chances.Data[i];
                 if (UnityEngine.Random.Range(0f, 1f) <= Data.Chance)
                 {
-                    Type = Data.Type;
-                    return;
+                    ToSet = Data.Type;
+                    break;
                 }
             }
-            Type = Chances.DefaultType;
+            Type = ToSet;
         }
+        GameObject aura = Auras[(int)Type];
+        if (aura)
+        {
+            aura.SetActive(true);
+        }
+    }
+    protected override void OnValidate()
+    {
+        base.OnValidate();
+        int count = Enum.GetValues(typeof(MysteryBoxType)).Length;
+        if (Auras.Length != count)
+        {
+            Auras = new GameObject[count];
+        }
+
     }
     protected override bool OnPicked(Collider other)
     {
